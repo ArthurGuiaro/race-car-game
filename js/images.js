03dd459c5c729d1459e2a424b36450b5ab1d747d
Arthur <arthurguiaro@gmail.com>
2023-08-29 20:25:48 -0300

    deploy test 2

--- js/images.js
+++ js/images.js
@@ -1,46 +1,46 @@
 let imgRoad = new Image()
-imgRoad.src = "./images/road.png"
+imgRoad.src = "images/road.png"
 
 let imgRoadLine = new Image()
-imgRoadLine.src = "./images/line.png"
+imgRoadLine.src = "images/line.png"
 
 let imgGasoline = new Image()
-imgGasoline.src ="./images/gasoline.png"
+imgGasoline.src ="images/gasoline.png"
 
 
 
 
 
 let imgBlueCar = new Image()
-imgBlueCar.src = "./images/cars/blue car.png"
+imgBlueCar.src = "images/cars/blue car.png"
 
 let imgGreenCar = new Image()
-imgGreenCar.src ="./images/cars/green car.png"
+imgGreenCar.src ="images/cars/green car.png"
 
 let imgPurpleCar = new Image()
-imgPurpleCar.src="./images/cars/purple car.png"
+imgPurpleCar.src="images/cars/purple car.png"
 
 let imgRedCar = new Image()
-imgRedCar.src= "./images/cars/red car.png"
+imgRedCar.src= "images/cars/red car.png"
 
 let imgYellowCar = new Image()
-imgYellowCar.src="./images/cars/yellow car.png"
+imgYellowCar.src="images/cars/yellow car.png"
 
 
 
 
 
 let imgGreenObstacle = new Image()
-imgGreenObstacle.src = "./images/cars/obstacles/green car.png"
+imgGreenObstacle.src = "images/cars/obstacles/green car.png"
 
 let imgLightYellowObstacle = new Image()
-imgLightYellowObstacle.src = "./images/cars/obstacles/light yellow car.png"
+imgLightYellowObstacle.src = "images/cars/obstacles/light yellow car.png"
 
 let imgPurpleObstacle = new Image()
-imgPurpleObstacle.src = "./images/cars/obstacles/purple car.png"
+imgPurpleObstacle.src = "images/cars/obstacles/purple car.png"
 
 let imgRedObstacle = new Image()
-imgRedObstacle.src = "./images/cars/obstacles/red car.png"
+imgRedObstacle.src = "images/cars/obstacles/red car.png"
 
 let imgYellowObstacle = new Image()
-imgYellowObstacle.src = "./images/cars/obstacles/yellow car.png"
+imgYellowObstacle.src = "images/cars/obstacles/yellow car.png"
